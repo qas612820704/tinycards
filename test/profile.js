167bd7428b772ac7a7a2c12700032922bfce2bb7
Lego <qas612820704@gmail.com>
2019-01-25 10:35:00 +0800

    v0.0.1

---
+++ test/profile.js
@@ -0,0 +1,348 @@
+export default {
+  creationDate: 1524677533,
+  email: 'qas612820704@gmail.com',
+  experiments: {
+    tinycards_android_personal_reminders: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_android_personal_reminders',
+      treated: false
+    },
+    tinycards_backend_daily_reminder_email: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_backend_daily_reminder_email',
+      treated: false
+    },
+    tinycards_backend_welcome_email: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_backend_welcome_email',
+      treated: false
+    },
+    tinycards_holdout_2017: {
+      condition: 'normal',
+      contexts: [],
+      destiny: 'normal',
+      eligible: false,
+      name: 'tinycards_holdout_2017',
+      treated: false
+    },
+    tinycards_holdout_2018_q2: {
+      condition: 'normal',
+      contexts: [],
+      destiny: 'normal',
+      eligible: false,
+      name: 'tinycards_holdout_2018_q2',
+      treated: false
+    },
+    tinycards_ios_adaptive_practice: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_ios_adaptive_practice',
+      treated: false
+    },
+    tinycards_ios_cards_per_lesson: {
+      condition: 'five',
+      contexts: [],
+      destiny: 'six',
+      eligible: false,
+      name: 'tinycards_ios_cards_per_lesson',
+      treated: false
+    },
+    tinycards_ios_cram_mode: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_ios_cram_mode',
+      treated: false
+    },
+    tinycards_ios_deck_typing_self_graded: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_ios_deck_typing_self_graded',
+      treated: false
+    },
+    tinycards_ios_featured_carousel: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_ios_featured_carousel',
+      treated: false
+    },
+    tinycards_ios_image_notifications: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_ios_image_notifications',
+      treated: false
+    },
+    tinycards_ios_quiz_mode: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_ios_quiz_mode',
+      treated: false
+    },
+    tinycards_ios_random_reminders: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_ios_random_reminders',
+      treated: false
+    },
+    tinycards_ios_remove_deck_strength: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_ios_remove_deck_strength',
+      treated: false
+    },
+    tinycards_ios_reorder_signup: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_ios_reorder_signup',
+      treated: false
+    },
+    tinycards_ios_simple_deck_creation: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_ios_simple_deck_creation',
+      treated: false
+    },
+    tinycards_ios_step_by_step_signup: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_ios_step_by_step_signup',
+      treated: false
+    },
+    tinycards_ios_temporarily_skip_typing: {
+      condition: 'multiple_choice',
+      contexts: [],
+      destiny: 'self_graded',
+      eligible: false,
+      name: 'tinycards_ios_temporarily_skip_typing',
+      treated: false
+    },
+    tinycards_web_android_pinned_decks: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_android_pinned_decks',
+      treated: false
+    },
+    tinycards_web_autosize_production_card: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_autosize_production_card',
+      treated: false
+    },
+    tinycards_web_categories_v2: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_categories_v2',
+      treated: false
+    },
+    tinycards_web_course_practice_button: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_course_practice_button',
+      treated: false
+    },
+    tinycards_web_default_cards_view: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_default_cards_view',
+      treated: false
+    },
+    tinycards_web_default_empty_card: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_default_empty_card',
+      treated: false
+    },
+    tinycards_web_drawing: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_drawing',
+      treated: false
+    },
+    tinycards_web_duolingo_deck_row: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_duolingo_deck_row',
+      treated: false
+    },
+    tinycards_web_home_redesign: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_home_redesign',
+      treated: false
+    },
+    tinycards_web_increase_char_limit: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_increase_char_limit',
+      treated: false
+    },
+    tinycards_web_mobile_deck_creation: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_mobile_deck_creation',
+      treated: false
+    },
+    tinycards_web_move_cover_description: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_move_cover_description',
+      treated: false
+    },
+    tinycards_web_recommended_decks: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_recommended_decks',
+      treated: false
+    },
+    tinycards_web_search_redesign: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_search_redesign',
+      treated: false
+    },
+    tinycards_web_show_card_count: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_show_card_count',
+      treated: false
+    },
+    tinycards_web_show_num_cards: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_show_num_cards',
+      treated: false
+    },
+    tinycards_web_simple_deck_creation: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_simple_deck_creation',
+      treated: false
+    },
+    tinycards_web_simplified_typing: {
+      condition: 'control',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_simplified_typing',
+      treated: false
+    },
+    tinycards_web_spreadsheet_mode: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_spreadsheet_mode',
+      treated: false
+    },
+    tinycards_web_survey: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'experiment',
+      eligible: false,
+      name: 'tinycards_web_survey',
+      treated: false
+    },
+    tinycards_web_temporarily_skip_typing: {
+      condition: 'experiment',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_temporarily_skip_typing',
+      treated: false
+    },
+    tinycards_web_xp: {
+      condition: '5xp',
+      contexts: [],
+      destiny: 'control',
+      eligible: false,
+      name: 'tinycards_web_xp',
+      treated: false
+    }
+  },
+  fullname: '江柏廷',
+  id: 384166125,
+  isNewFromDuolingo: false,
+  learningLanguage: 'zh',
+  newCourseImports: [],
+  picture:
+    'https: //duolingo-images.s3.amazonaws.com/avatars/384166125/xoy-r5qReH/xlarge',
+  subscribed: false,
+  subscriberCount: 0,
+  subscriptionCount: 0,
+  trackingProperties: {},
+  uiLanguage: 'en',
+  username: 'xKvD13',
+  welcomeDate: 1524677720,
+  cookie: {
+    jwtToken:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NDQ3NTM0MzksInN1YiI6Mzg0MTY2MTI1LCJleHAiOjQ2OTgzNTM0Mzl9.5TsPo6gKj-XcOelC0WF56uQpmbDSBJJ43Hg99xJFXJw',
+    Domain: '.duolingo.com',
+    Expires: 'Sat, 14-Dec - 2019 03: 17: 19 GMT',
+    'Max-Age': '31540000.0',
+    Path: '/'
+  }
+}
